d2390b9ff7d1a70f1fb0fb0c533b61b98c71d357
marvieiradev <marcosvieira482@gmail.com>
2024-11-12 00:11:52 -0300

    feat: ensure transaction can be updated

--- app/transactions/_components/edit-transaction-button.tsx
+++ app/transactions/_components/edit-transaction-button.tsx
@@ -1,7 +1,7 @@
 "use client";
-/*
+
 import { Button } from "@/app/_components/ui/button";
-//import UpsertTransactionDialog from "@/app/_components/upsert-transaction-dialog";
+import UpsertTransactionDialog from "@/app/_components/upsert-transaction-dialog";
 import { Transaction } from "@prisma/client";
 import { PencilIcon } from "lucide-react";
 import { useState } from "react";
@@ -36,4 +36,4 @@ const EditTransactionButton = ({ transaction }: EditTransactionButtonProps) => {
   );
 };
 
-export default EditTransactionButton;*/
+export default EditTransactionButton;
