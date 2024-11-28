7a5f23c770ed8743f4cde151c1505ad20184110d
marvieiradev <marcosvieira482@gmail.com>
2024-11-27 22:32:48 -0300

    feat: add stripe webhook that handles subscriptions

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,15 +1,19 @@
-import { auth } from "@clerk/nextjs/server";
+import { auth, clerkClient } from "@clerk/nextjs/server";
 import Navbar from "../_components/navbar";
 import { redirect } from "next/navigation";
 import { Card, CardContent, CardHeader } from "../_components/ui/card";
 import { CheckIcon, XIcon } from "lucide-react";
 import AcquirePlanButton from "./_components/acquire-plan-button";
+import { Badge } from "../_components/ui/badge";
 
 const SubscriptionPage = async () => {
   const { userId } = await auth();
   if (!userId) {
     redirect("/login");
   }
+
+  const user = await clerkClient().users.getUser(userId);
+  const hasPremiumPlan = user?.publicMetadata.subscriptionPlan == "premium";
   return (
     <>
       <Navbar />
@@ -47,7 +51,12 @@ const SubscriptionPage = async () => {
           </Card>
 
           <Card className="w-[458px]">
-            <CardHeader className="text-center text-2xl font-semibold">
+            <CardHeader className="relative text-center text-2xl font-semibold">
+              {hasPremiumPlan && (
+                <Badge className="absolute left-4 top-9 bg-primary/10 text-primary">
+                  Ativo
+                </Badge>
+              )}
               <h2 className="text-center text-2xl font-semibold">
                 Plano Premium
               </h2>
