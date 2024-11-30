ada181248ba7ef1c0794df458fbfc8d8e436d73f
marvieiradev <marcosvieira482@gmail.com>
2024-11-29 23:02:04 -0300

    feat: ensure only premium users can generate ai report

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,4 +1,4 @@
-import { auth } from "@clerk/nextjs/server";
+import { auth, clerkClient } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import Navbar from "../_components/navbar";
 import SummaryCards from "./_components/summary-cards";
@@ -31,6 +31,7 @@ const Home = async ({ searchParams: { month } }: HomeProps) => {
   }
 
   const userCanAddTransaction = await canUserAddTransaction();
+  const user = await clerkClient().users.getUser(userId);
 
   const dashboard = await getDashboard(month);
   return (
@@ -40,7 +41,12 @@ const Home = async ({ searchParams: { month } }: HomeProps) => {
         <div className="flex justify-between">
           <h1 className="text-2xl font-bold">Dashboard</h1>
           <div className="flex items-center gap-3">
-            <AiReportsButton hasPremiumPlan={true} month={month} />
+            <AiReportsButton
+              month={month}
+              hasPremiumPlan={
+                user.publicMetadata.subscriptionPlan === "premium"
+              }
+            />
             <TimeSelect />
           </div>
         </div>
